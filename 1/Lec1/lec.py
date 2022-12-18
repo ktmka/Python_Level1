9cea1b09a7a0c22e3f6984007fe022ec1e26aa68
ktmka <tomatimoto@gmail.com>
2022-12-18 20:05:00 +0500

    добавили папку по Семинару 1

---
+++ 1/Lec1/lec.py
@@ -0,0 +1,61 @@
+# x = 123
+# y = 7.65
+# # z = None
+# print(x)
+# print(type(y))
+# str = 'ggg'
+# print("введите число")
+# a = int(input('a = ' ))
+# # print("введите число")
+# b = int(input('b = '))
+# print(a)
+# print(b)
+# print(a + b)
+# print(f'{a}, {b}')
+# ---------------------------------
+# original = 23
+# inverted = 0
+# while original != 0:
+#     inverted = inverted * 10 + (original % 10)
+#     original //= 10
+# else:
+#     print('достаточно')
+# print(inverted)
+
+# for i in 1,2,3,4,5:
+#     print(i**2)
+
+# list = [1,2,3,4,5]
+# for i in list:
+#     print(i**2)
+
+# r = range(10)
+# for i in r:
+#     print(i)
+
+# r = range(1,10,2)
+# for i in r:
+#     print(i)
+    
+# for i in 'qwertt':
+#     print(i)
+
+# r = range(1,10)
+# numbers = list(r)   #приведение типа range к типу list
+
+# def Vagon():
+#     i = int(input())
+#     j = int(input())
+#     result = i + j - 1
+#     print(result)
+
+# Vagon()
+
+# year = int(input("input year = "))
+# if (year%100 != 0) and (year%400 == 0 or year%4 == 0):
+#     print("YES")
+# else:
+#     print("NO")
+
+    
+
